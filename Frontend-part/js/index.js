ed5269a46017db513999c775a2ed595d1756e9d0
Samivulla khan <114461462+Samikhan34177@users.noreply.github.com>
2023-09-21 23:55:56 +0100

    Update index.js

--- Frontend-part/js/index.js
+++ Frontend-part/js/index.js
@@ -1,8 +1,21 @@
 const counter = document.querySelector(".counter-number");
+let countValue;
+
 async function updateCounter() {
-    let response = await fetch("https://pljre3bfb2w2l7p3k3z2kp22ie0txdew.lambda-url.us-east-1.on.aws/Your-lambdaFunction-URL");
-    let data = await response.json();
-    counter.innerHTML = `This page has ${counterData} Views!`;
+    try {
+        const response = await fetch("https://pljre3bfb2w2l7p3k3z2kp22ie0txdew.lambda-url.us-east-1.on.aws/Your-lambdaFunction-URL");
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const data = await response.json();
+        countValue = data.views; // Assuming the data structure includes a 'views' property
+        
+        counter.innerHTML = `This page has ${countValue} Views!`;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 updateCounter();
