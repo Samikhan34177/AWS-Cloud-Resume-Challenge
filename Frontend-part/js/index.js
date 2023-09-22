009109a4086e27704b3079c0be8a68b916e41572
Samivulla khan <114461462+Samikhan34177@users.noreply.github.com>
2023-09-22 20:48:51 +0100

    Update index.js

--- Frontend-part/js/index.js
+++ Frontend-part/js/index.js
@@ -1,21 +1,9 @@
 const counter = document.querySelector(".counter-number");
-let countValue;
 
 async function updateCounter() {
-    try {
-        const response = await fetch("https://437cfskspj.execute-api.us-east-1.amazonaws.com/endpoint");
-        
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        
-        const data = await response.json();
-        countValue = data.views; // Assuming the data structure includes a 'views' property
-        
-        counter.innerHTML = `This page has ${countValue} Views!`;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    const response = await fetch("https://437cfskspj.execute-api.us-east-1.amazonaws.com/endpoint");
+    const data = await response.json();
+    counter.innerHTML = `This page has ${data} views!`;
 }
 
 updateCounter();
